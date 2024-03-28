216d6fc6689e3f28f040334f701c6850db1cf0f9
guoruipan <grpzaragoza@gmail.com>
2024-03-28 16:32:28 +0100

    Installed tw-elements-react
    npm install tw-elements-react

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,29 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-orange-700 p-4 text-white text-center mt-auto">
-        {/* footer (mt-auto), junto a body (min-h-screen flex flex-col) empujan el footer al fondo */}
-      <div className="mb-4">
+    <footer className="bg-orange-700 p-4 text-white mt-auto">
+      {/* footer (mt-auto), junto a body (min-h-screen flex flex-col) empujan el footer al fondo */}
+      <div className="mb-4 text-center">
         &copy; {new Date().getFullYear()} - Todos los derechos reservados.
       </div>
-      <ul className="flex justify-center space-x-4">
-        <li>
-          <Link href="/politica-privacidad">Política de privacidad</Link>
-        </li>
-        <li>
-          <Link href="/terminos-condiciones">Términos y condiciones</Link>
-        </li>
-      </ul>
+
+      <div className="flex justify-between px-10 items-center">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/politica-privacidad">Política de privacidad</Link>
+          </li>
+          <li>
+            <Link href="/terminos-condiciones">Términos y condiciones</Link>
+          </li>
+        </ul>
+        <ul className="flex space-x-4">
+          {/* https://tw-elements.com/docs/standard/components/social-buttons/ */}
+          <li>
+          </li>
+          <li>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements-react/dist/plugin.cjs")],
 };
 export default config;
