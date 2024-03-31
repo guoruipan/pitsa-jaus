ddbd2624b572c5d64685f4755a3a178c0ff9d850
guoruipan <grpzaragoza@gmail.com>
2024-03-31 12:38:37 +0200

    Configured mui
    Added mui and emotion dependencies. Created theme.ts. Updated layout.tsx to integrate mui with next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,20 @@
+// AppRouterCacheProvider es recomendable para trabajar con mui
+// https://mui.com/material-ui/integrations/nextjs/
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "#/components/Nav";
 import Footer from "#/components/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "#/theme";
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | PitsaJaus',
-    default: 'PitsaJaus',
+    template: "%s | PitsaJaus",
+    default: "PitsaJaus",
   },
-  description: "¡Sabor auténtico en cada bocado! PitsaJaus, donde la tradición se fusiona con el sabor.",
+  description:
+    "¡Sabor auténtico en cada bocado! PitsaJaus, donde la tradición se fusiona con el sabor.",
 };
 
 export default function RootLayout({
@@ -21,13 +24,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen flex flex-col`}>
-        <Nav />
-        <main className="container mx-auto my-6 px-10 md:px-0">
-          {/* aplico más padding cuando la pantalla es pequeñs */}
-          {children}
-        </main>
-        <Footer />
+      <body className={`min-h-screen flex flex-col`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <Nav />
+            <main className="container mx-auto my-6 px-10 md:px-0">
+              {/* aplico más padding cuando la pantalla es pequeñs */}
+              {children}
+            </main>
+            <Footer />
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
