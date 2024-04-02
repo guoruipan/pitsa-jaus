de7dbf7d333d01426861bd58506859e9a5390495
Guo <159023824+guoruipan@users.noreply.github.com>
2024-04-02 16:54:41 +0200

    Fixed key issue in /menu

--- app/(home)/menu/page.tsx
+++ app/(home)/menu/page.tsx
@@ -2,7 +2,7 @@ import Card from "#/components/ui/Card";
 import Grid from "@mui/material/Grid";
 import { list as listPizzas } from "#/models/pizza";
 import { Metadata } from "next";
-import PageTitle from '#/components/ui/PageTitle';
+import PageTitle from "#/components/ui/PageTitle";
 
 const pageTitle = "Nuestra Carta";
 
@@ -21,12 +21,8 @@ export default async function Page() {
       </Grid>
 
       {pizzas.map((pizza) => (
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Card
-            key={pizza.id}
-            imgUrl="/placeholder.jpg"
-            text={`Pizza ${pizza.name}`}
-          />
+        <Grid key={pizza.id} item xs={12} sm={6} md={4} lg={3}>
+          <Card imgUrl="/placeholder.jpg" text={`Pizza ${pizza.name}`} />
         </Grid>
       ))}
     </Grid>
