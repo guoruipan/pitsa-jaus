cd74823090e4b775edb0a2472ded004cf5bb3d7e
guoruipan <grpzaragoza@gmail.com>
2024-05-18 12:16:54 +0200

    refactor: homepage
    separated it into xs and sm version. more readable for me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,47 @@
 import React from "react";
 import Image from "next/image";
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Stack, SxProps, Theme, Typography } from "@mui/material";
 import photo from "@/public/logo3.png";
+import NextLink from "next/link";
 
 // https://nextjs.org/docs/app/building-your-application/optimizing/images#responsive
 
 export default async function Home() {
   return (
-    <Grid container spacing={2}>
-      <Grid
-        item
-        xs={12}
-        display={{ xs: "flex", sm: "none" }}
-        justifyContent={"center"}
-      >
-        <Typography variant="h4">Felicidad en cada bocado</Typography>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={4}
-        sx={{ display: "flex", flexDirection: "column" }}
-      >
+    <>
+      <HomeXS />
+      <HomeMD />
+    </>
+  );
+}
+
+function HomeXS() {
+  return (
+    <Stack
+      spacing={2}
+      display={{ xs: "flex", sm: "none" }}
+      justifyContent={"center"}
+      textAlign={"center"}
+    >
+      <Typography variant="h4">Felicidad en cada bocado</Typography>
+      <Image
+        src={photo}
+        sizes="100vw"
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+        alt="Logo pitsajaus"
+      />
+      <OrderNowButton />
+    </Stack>
+  );
+}
+
+function HomeMD() {
+  return (
+    <Grid container spacing={2} display={{ xs: "none", sm: "flex" }}>
+      <Grid item xs={12} sm={4}>
         <Image
           src={photo}
           sizes="100vw"
@@ -32,30 +52,26 @@ export default async function Home() {
           alt="Imagen logo"
         />
       </Grid>
-      <Grid
-        item
-        xs={12}
-        display={{ xs: "flex", sm: "none" }}
-        justifyContent={"center"}
-      >
-        <Button variant="contained">Pide ya</Button>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={8}
-        display={{ xs: "none", sm: "flex" }}
-        alignItems={"center"}
-      >
-        <Box>
-          <Typography variant="h4">
-            PitsaJaus, donde la tradición se fusiona con el sabor.
-          </Typography>
-          <Button variant="contained" sx={{ px: 3, mt: 3 }}>
-            Pide ya
-          </Button>
-        </Box>
+      <Grid item xs={12} sm={8} alignContent={"center"}>
+        <Typography variant="h4">
+          PitsaJaus, donde la tradición se fusiona con el sabor.
+        </Typography>
+        <OrderNowButton sx={{ px: 3, mt: 3 }} />
       </Grid>
     </Grid>
+  );
+}
+
+function OrderNowButton({ sx }: { sx?: SxProps<Theme> }) {
+  // gracias a middleware, si intento entrar a dashboard sin sesión iniciada redirige a login
+  return (
+    <Button
+      href="/dashboard/cart"
+      component={NextLink}
+      variant="contained"
+      sx={{ ...sx }}
+    >
+      Pide ya
+    </Button>
   );
 }
